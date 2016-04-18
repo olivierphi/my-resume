5715e6891540228d40fea1fdba8b7364f5467b54
DrBenton <olivier@rougemine.com>
2016-04-18 19:48:37 +0200

    [Container] Added optional DIC cache

--- src/Rougemine/Resume/ContainerBuilder.php
+++ src/Rougemine/Resume/ContainerBuilder.php
@@ -3,13 +3,17 @@
 namespace Rougemine\Resume;
 
 use Rougemine\Resume\DependencyInjection\TranslationsCompilerPass;
+use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ContainerBuilder
 {
+    const USE_CACHE = false;
+    
     const SERVICES_DIR = '%app.dir%/src/Rougemine/Resume/Resources/config';
 
     /**
@@ -30,16 +34,46 @@ class ContainerBuilder
      */
     public function getContainer()
     {
+        static $container;
+        
+        if (null !== $container) {
+            return $container;
+        }
+        
+        if (!static::USE_CACHE) {
+            // No cache? This is simple!
+            $container = $this->getNewContainer();
+            
+            return $container;
+        }
+        
+        // Cached container? It's gonna be just a little bit more complex.
+        $containerCacheFilePath = $this->appDir . '/var/tmp/container.php';
+        $containerConfigCache = new ConfigCache($containerCacheFilePath, true);
+
+        if (!$containerConfigCache->isFresh()) {
+            $containerBuilder  = $this->getNewContainer();
+
+            $dumper = new PhpDumper($containerBuilder);
+            $containerConfigCache->write(
+                $dumper->dump(),
+                $containerBuilder->getResources()
+            );
+        }
+
+        require_once $containerCacheFilePath;
+        $container = new \ProjectServiceContainer();
+        
+        return $container;
+    }
+    
+    private function getNewContainer()
+    {
         $servicesDir = str_replace('%app.dir%', $this->appDir, self::SERVICES_DIR);
 
         $container = new SymfonyContainerBuilder();
 
-        // Some runtime parameters...
-        $container->setParameter('app.dir', $this->appDir);
-        $prodMode = getenv('PROD_MODE') ? true : false;
-        $container->setParameter('app.prodMode', $prodMode);
-        $googleAnalyticsTrackingCode = getenv('GA_TRACKING_CODE') ?: null;
-        $container->setParameter('app.google.analytics.trackingCode', $googleAnalyticsTrackingCode);
+        $this->setRuntimeParameters($container);
 
         // Services definitions loading
         $loader = new YamlFileLoader($container, new FileLocator($servicesDir));
@@ -52,5 +86,21 @@ class ContainerBuilder
         $container->compile();
 
         return $container;
+    }
+
+    /**
+     * @param SymfonyContainerBuilder $containerBuilder
+     */
+    private function setRuntimeParameters(SymfonyContainerBuilder $containerBuilder)
+    {
+        // Some runtime parameters...
+        $containerBuilder->setParameter('app.dir', $this->appDir);
+        $containerBuilder->setParameter('app.tmp.dir', $this->appDir . '/var/tmp');
+
+        $prodMode = getenv('PROD_MODE') ? true : false;
+        $containerBuilder->setParameter('app.prodMode', $prodMode);
+
+        $googleAnalyticsTrackingCode = getenv('GA_TRACKING_CODE') ?: null;
+        $containerBuilder->setParameter('app.google.analytics.trackingCode', $googleAnalyticsTrackingCode);
     }
 }
