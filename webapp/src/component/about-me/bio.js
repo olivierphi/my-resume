ea5486d7bd61d3a1b3b1a3cd1e9c76357b501fee
Olivier Philippon <olivier@rougemine.com>
2017-10-29 18:44:28 +0000

    Add "moment.js" for date formatting

--- webapp/src/component/about-me/bio.js
+++ webapp/src/component/about-me/bio.js
@@ -1,9 +1,16 @@
+import moment from "moment";
 import React from "react";
 import PropTypes from "prop-types";
 import HeaderIcon from "component/header-icon";
 import UserIcon from "component/header-icons/user.svg";
 
 export default class Bio extends React.Component {
+  constructor() {
+    super();
+
+    this.formatDate = this.formatDate.bind(this);
+  }
+
   render() {
     return (
       <section className="me-container">
@@ -14,9 +21,15 @@ export default class Bio extends React.Component {
         <p className="rich-snippet-only">
           <span itemProp="jobTitle">{this.props.bio.jobTitle}</span>
         </p>
-        <p>{this.props.i18n.me.birthDate}</p>
+        <p>
+          {this.props.i18n.me.birthDate} {this.formatDate(this.props.bio.birth)}
+        </p>
       </section>
     );
+  }
+
+  formatDate(/** String */ dateYMD) {
+    return moment(dateYMD, "YYYY-MM-DD").format(this.props.i18n.date_format);
   }
 }
 
@@ -24,8 +37,10 @@ Bio.propTypes = {
   bio: PropTypes.shape({
     name: PropTypes.string.isRequired,
     jobTitle: PropTypes.string.isRequired,
+    birth: PropTypes.string.isRequired,
   }).isRequired,
   i18n: PropTypes.shape({
+    date_format: PropTypes.string.isRequired,
     me: PropTypes.shape({
       birthDate: PropTypes.string.isRequired,
     }).isRequired,
